7f4ab17275dd5d3c0da56aad4a4fb8622f5c935a
kalijerry <729913310@qq.com>
2024-11-12 11:30:54 +0800

    加入侧边栏

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Layout , Menu, Breadcrumb,theme } from 'antd';
-const { Header, Content, Footer } = Layout;
-const currentYear = new Date().getFullYear(); //版权年份
+import AppMenuLeft from './tsx/menu_left';
 
+const { Header, Content, Footer, Sider } = Layout;
+const currentYear = new Date().getFullYear(); //版权年份
 const items = [
     {
         label: '主页',
@@ -22,6 +23,7 @@ const items = [
 ];
 
 
+
 const App: React.FC = () => {
     const {
         token: {colorBgContainer, borderRadiusLG},
@@ -29,7 +31,6 @@ const App: React.FC = () => {
     return (
         <div className="App">
             <Layout>
-
                 <Header style={{display: 'flex', alignItems: 'center'}}>
                     <h1 style={{ color: 'white' }}>
                         PBlaze
@@ -43,11 +44,14 @@ const App: React.FC = () => {
                         style={{flex: 1, minWidth: 0}}
                     />
                 </Header>
+            <Layout>
+                <Sider width={200} style={{background: colorBgContainer }}> {/* 使用 Sider 组件 */}
+                    <AppMenuLeft /> {/*  渲染左侧菜单组件 */}
+                </Sider>
+            <Layout style={{ padding: '0 24px 24px' }}>
                 <Content style={{padding: '0 48px'}}>
                     <Breadcrumb style={{margin: '16px 0'}}>
-                        <Breadcrumb.Item>Home</Breadcrumb.Item>
-                        <Breadcrumb.Item>List</Breadcrumb.Item>
-                        <Breadcrumb.Item>App</Breadcrumb.Item>
+                        <Breadcrumb.Item>主页</Breadcrumb.Item>
                     </Breadcrumb>
                     <div
                         style={{
@@ -56,15 +60,23 @@ const App: React.FC = () => {
                             padding: 24,
                             borderRadius: borderRadiusLG,
                         }}
-                    >Content
+                    >
+                        <h1>欢迎来到PBlaze</h1>
+                        <p>这是一个基于React的前端项目</p>
+                        <p>项目地址：<a href='https://github.com/zdwtest/pblaze_website'>Github.com</a>
+                        </p>
+
                     </div>
                 </Content>
                 <Footer style={{ textAlign: 'center' }}>
                     <p>Copyright © 2024-{currentYear} KaliJerry. Licensed under the <a href="/LICENSE">Apache License 2.0</a> PBlaze.com.</p>
                 </Footer>
+                    </Layout>
+                </Layout>
             </Layout>
         </div>
     );
 };
+
 
 export default App;
